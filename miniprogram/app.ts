2067122146d13b615923e6fbae501b4b1dd8bab1
linyihan <515461402@qq.com>
2025-12-11 14:41:32 +0800

    feat：删除logs相关页面

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -7,10 +7,6 @@ App<IAppOption>({
     code: ''
   },
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
